4e08e22922099c7776848c30d02173867a53eb85
Jorge Moñiz <jorgemoniz@live.com>
2017-04-05 21:52:07 +0200

    AddNewGame // DataModel // DatePicker // PickerPhoto // KeyboardFeatures // Utils

--- iRullo/iRAddNewGameViewController.swift
+++ iRullo/iRAddNewGameViewController.swift
@@ -7,29 +7,269 @@
 //
 
 import UIKit
+import CoreData
+
+// Si la vista anterior dice que ha añadido un juego, hace una vista de recarga
+protocol iRAddNewGameViewControllerDelegate {
+    func didAddGame()
+}
 
 class iRAddNewGameViewController: UIViewController {
 
+    //MARK: - Variables locales globales
+    var manageContext : NSManagedObjectContext!
+    var iRDelegate : iRAddNewGameViewControllerDelegate?
+    var game : Game?
+    var datePicker : UIDatePicker!
+    var dateFormatter = DateFormatter()
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var myImageGame: UIImageView!
+    @IBOutlet weak var mySwitchBorrowed: UISwitch!
+    @IBOutlet weak var myTituloGame: UITextField!
+    @IBOutlet weak var myQuienPrestadoGame: UITextField!
+    @IBOutlet weak var myCuandoPrestadoGame: UITextField!
+    @IBOutlet weak var myEliminarJuegoBTN: UIButton!
+    
+    //MARK: - IBActions
+    @IBAction func mySwitchChangedValue(_ sender: UISwitch) {
+        if sender.isOn {
+            myQuienPrestadoGame.isEnabled = true
+            myCuandoPrestadoGame.isEnabled = true
+            myCuandoPrestadoGame.text = dateFormatter.string(from: Date())
+        } else {
+            myQuienPrestadoGame.isEnabled = false
+            myCuandoPrestadoGame.isEnabled = false
+            myQuienPrestadoGame.text = ""
+            myCuandoPrestadoGame.text = ""
+        }
+    }
+    
+    @IBAction func myEliminarJuegoACT(_ sender: UIButton) {
+        if let context = manageContext {
+            context.delete(game!)           //El que se está mostrando en pantalla
+            game = nil
+            iRDelegate?.didAddGame()
+            _ = navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    //MARK: - LIFE VC
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        //Formato de fecha
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        
+        //Imagen
+        myImageGame.isUserInteractionEnabled = true
+        let tapGR = UITapGestureRecognizer(target: self, action: #selector(pickerPhoto))
+        myImageGame.addGestureRecognizer(tapGR)
+        
+        //Teclado
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow(_:)),
+                                               name: NSNotification.Name.UIKeyboardWillShow,
+                                               object: nil)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide(_:)),
+                                               name: NSNotification.Name.UIKeyboardWillHide,
+                                               object: nil)
+        
+        //Esconder el teclado
+        let tapGRhidekeyboard = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        self.view.addGestureRecognizer(tapGRhidekeyboard)
+        
+        //DatePicker
+        datePicker = UIDatePicker(frame: CGRect(x: 0, y: 210, width: 320, height: 216))
+        datePicker.datePickerMode = .date
+        datePicker.addTarget(self, action: #selector(datePickerValueChanged(_:)), for: .valueChanged)
+        myCuandoPrestadoGame.inputView = datePicker
+        
+        
+        //Dos lógicas para el mismo VC
+        //      1. Add / 2. Edit
+        if game == nil {
+            self.title = "Añadir Videojuego"
+            self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelButtonAction))
+            
+            self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveButtonAction))
+            
+            myEliminarJuegoBTN.isHidden = true
+            mySwitchBorrowed.isOn = false
+        } else {
+            self.title = "Editar Videojuego"
+            myTituloGame.text = game?.title
+            
+            if let borrowed = game?.borrowed {
+                mySwitchBorrowed.isOn = borrowed
+            }
+            myQuienPrestadoGame.text = game?.borrowedTo
+            if let borrowedDate = game?.borrowedDate as Date? {
+                myCuandoPrestadoGame.text = dateFormatter.string(from: borrowedDate)
+            }
+            if let imageData = game?.image as Data? {
+                myImageGame.image = UIImage(data: imageData)
+            }
+            myEliminarJuegoBTN.isHidden = false
+        }
+        
+        if !mySwitchBorrowed.isOn {
+            myQuienPrestadoGame.isEnabled = false
+            myCuandoPrestadoGame.isEnabled = false
+        }
         // Do any additional setup after loading the view.
     }
-
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        if game != nil {
+            saveGame()
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //MARK: - Utils
+    func keyboardWillShow(_ notification : Notification) {
+        let info = notification.userInfo!
+        let keyboardFrame = (info [UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let keyboardTime = (info[UIKeyboardAnimationDurationUserInfoKey] as! NSNumber).doubleValue
+        
+        //Animation - La vista sube tanto como el keyboard.
+        UIView.animate(withDuration: keyboardTime) { 
+            self.view.frame.origin.y = -(keyboardFrame.height)
+        }
     }
-    */
+    
+    func keyboardWillHide(_ notification : Notification) {
+        let info = notification.userInfo!
+        let keyboardTime = (info[UIKeyboardAnimationDurationUserInfoKey] as! NSNumber).doubleValue
+        
+        UIView.animate(withDuration: keyboardTime) {
+            self.view.frame.origin.y = 0
+        }
+    }
+    
+    func hideKeyboard() {
+        for c_view in self.view.subviews {
+            if let textField = c_view as? UITextField {
+                textField.resignFirstResponder()
+                //Dimite como primer respondedor
+            }
+        }
+    }
+    
+    func datePickerValueChanged(_ picker : UIDatePicker) {
+        myCuandoPrestadoGame.text = dateFormatter.string(from: picker.date)
+    }
+    
+    func cancelButtonAction() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func saveButtonAction() {
+        saveGame()
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func saveGame() {
+        //Inyectamos el contexto
+        if let context = manageContext {
+            //1
+            var editedGame : Game?
+            
+            if game == nil {
+                editedGame = Game(context: context)
+            } else {
+                editedGame = game
+            }
+            
+            editedGame?.title = self.myTituloGame.text
+            editedGame?.borrowed = self.mySwitchBorrowed.isOn
+            
+            let imageData = UIImageJPEGRepresentation(myImageGame.image!, 0.3)
+            editedGame?.image = imageData as NSData?
+            
+            if (editedGame?.borrowed)! {
+                editedGame?.borrowedTo = myQuienPrestadoGame.text?.uppercased()
+                editedGame?.borrowedDate = dateFormatter.date(from: myCuandoPrestadoGame.text!) as NSDate?
+            }
+            
+            do {
+                try context.save()
+                self.iRDelegate?.didAddGame()
+                //si todo se ha guardado correctamente, se añade un nuevo juego
+            } catch {
+                print("Error al guardar los datos en CoreData")
+            }
+        }
+    }
+    
+    //Por si acaso
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 
+}
+
+//SNIPPET - myPickerPhoto
+extension iRAddNewGameViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    func pickerPhoto(){
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            muestraMenu()
+        }else{
+            muestraLibreriaFotos()
+        }
+    }
+    
+    func muestraMenu(){
+        let alertVC = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        let tomaFotoAction = UIAlertAction(title: "Toma Foto", style: .default) { _ in
+            self.muestraCamara()
+        }
+        let seleccionaLibreriaAction = UIAlertAction(title: "Selecciona de la Librería", style: .default) { _ in
+            self.muestraLibreriaFotos()
+        }
+        alertVC.addAction(cancelAction)
+        alertVC.addAction(tomaFotoAction)
+        alertVC.addAction(seleccionaLibreriaAction)
+        present(alertVC, animated: true, completion: nil)
+    }
+    
+    func muestraCamara(){
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .camera
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    func muestraLibreriaFotos(){
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let imageData = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            myImageGame.image = imageData
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
 }
--- iRullo/iRGameListViewController.swift
+++ iRullo/iRGameListViewController.swift
@@ -7,11 +7,25 @@
 //
 
 import UIKit
+import CoreData
 
 class iRGameListViewController: UIViewController {
-
+    
+    //MARK: - Variables locales
+    var manageContext : NSManagedObjectContext!
+    var listGame = [Game]()
+    
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var myFilterSegmentCon: UISegmentedControl!
+    @IBOutlet weak var myCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        myCollectionView.delegate = self
+        myCollectionView.dataSource = self
+        myCollectionView.alwaysBounceVertical = true
 
         // Do any additional setup after loading the view.
     }
@@ -22,14 +36,65 @@ class iRGameListViewController: UIViewController {
     }
     
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+        
+        if segue.identifier == "addGameSegue" {
+            let navVC = segue.destination as! UINavigationController
+            let detalleVC = navVC.topViewController as! iRAddNewGameViewController
+        }
+        
+        if segue.identifier == "editGameSegue" {
+            let detalleVC = segue.destination as! iRAddNewGameViewController
+        }
 
+    }
+ 
+
+} //TODO: - fin de de la clase
+
+extension iRGameListViewController : UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        if listGame.count == 0 {
+            let imageBackgroundList = UIImageView(image: #imageLiteral(resourceName: "img_empty_list"))
+            imageBackgroundList.contentMode = .scaleAspectFit
+            myCollectionView.backgroundView = imageBackgroundList
+            
+        } else {
+            myCollectionView.backgroundView = UIView()
+        }
+        return listGame.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let customCell = myCollectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        
+        
+        
+        
+        return customCell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "editGameSegue", sender: self)
+        
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        let offsetY = scrollView.contentOffset.y
+        if (offsetY < -120) {
+            performSegue(withIdentifier: "addGameSegue", sender: self)
+        }
+    }
+    
 }
